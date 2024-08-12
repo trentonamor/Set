650c01a4fd3326d28591f0fea56c3ca4adfae840
Trenton Parrotte <tparrotte@alarm.com>
2024-08-12 14:51:00 -0600

    Assignment 3: Initial Commit

---
+++ Spot/Spot/SpotApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotApp.swift
+//  Spot
+//
+//  Created by Trenton Parrotte on 8/12/24.
+//
+
+import SwiftUI
+
+@main
+struct SpotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
